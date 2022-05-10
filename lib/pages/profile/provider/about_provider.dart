d43ab549a587b418f6d123723cf8f9e2f343beb7
Asylzhan <asylzhan1809200@gmail.com>
2022-05-10 12:49:58 +0600

    qwe

--- lib/pages/profile/provider/about_provider.dart
+++ lib/pages/profile/provider/about_provider.dart
@@ -1,3 +1,25 @@
+import 'package:flutter/material.dart';
 import 'package:kaz_med/base/base_bloc.dart';
+import 'package:kaz_med/shared/size_config.dart';
 
-class AboutProvider extends BaseBloc {}
+class AboutProvider extends BaseBloc {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController surnameController = TextEditingController();
+  TextEditingController birthController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+
+  Size? size;
+
+  init(context) {
+    setLoading(true);
+    size = MediaQuery.of(context).size;
+    SizeConfig().init(context);
+    nameController.text = 'Aidana';
+    surnameController.text = 'Kenes';
+    emailController.text = 'kenesaidana@gmail.com';
+    notifyListeners();
+    setLoading(false);
+  }
+}
